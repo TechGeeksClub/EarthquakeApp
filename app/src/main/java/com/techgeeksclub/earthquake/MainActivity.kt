3ebd53ab51c216ea749d0c570cff81494bdd5003
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-06-14 22:17:25 +0300

    setted bottom navigation view

--- app/src/main/java/com/techgeeksclub/earthquake/MainActivity.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/MainActivity.kt
@@ -2,15 +2,31 @@ package com.techgeeksclub.earthquake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.techgeeksclub.earthquake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView : BottomNavigationView
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation(){
+        bottomNavigationView = binding.bottomNavigation
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
 }
