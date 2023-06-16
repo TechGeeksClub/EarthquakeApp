4ba63a78ac7f5b2a4a6f52be96b1d1e1410b44e2
Hamza Oban <56612937+HamzaOban@users.noreply.github.com>
2023-06-17 00:57:01 +0300

    Made adjustments to viewBinding.

--- app/src/main/java/com/techgeeksclub/earthquake/ui/home/HomeFragment.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/home/HomeFragment.kt
@@ -1,23 +1,34 @@
 package com.techgeeksclub.earthquake.ui.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.techgeeksclub.earthquake.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.techgeeksclub.earthquake.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
-
-
     private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        // RecyclerView'i ayarlayın
+        // RecyclerView'i ayarlayın
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
