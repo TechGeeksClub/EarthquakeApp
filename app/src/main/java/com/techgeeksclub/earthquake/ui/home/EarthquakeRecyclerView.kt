d6aa22b55bed709f4b77b0370b5678c37b5c7e75
Hamza Oban <56612937+HamzaOban@users.noreply.github.com>
2023-08-06 14:47:38 +0300

    added RecyclerView primary constructor.

--- app/src/main/java/com/techgeeksclub/earthquake/ui/home/EarthquakeRecyclerView.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/home/EarthquakeRecyclerView.kt
@@ -1,19 +1,50 @@
 package com.techgeeksclub.earthquake.ui.home
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.example.example.Result
 import com.techgeeksclub.earthquake.R
+import com.techgeeksclub.earthquake.model.Earthquake
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
-class EarthquakeRecyclerView : RecyclerView.Adapter<EarthquakeRecyclerView.EarthquakeViewHolder>() {
+@RequiresApi(Build.VERSION_CODES.O)
+class EarthquakeRecyclerView(val earthquake : ArrayList<Earthquake>) : RecyclerView.Adapter<EarthquakeRecyclerView.EarthquakeViewHolder>() {
+    var result : ArrayList<Result> = arrayListOf()
+    var howManyMinutes : ArrayList<String> = arrayListOf()
+    var intensity : ArrayList<Double> = arrayListOf()
+    var country : ArrayList<String> = arrayListOf()
+
+    init {
+        earthquake.forEach {
+            result = it.result
+            it.result.forEach {
+                val dateNow = LocalDateTime.now()
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                val formatted = dateNow.format(formatter)
+
+
+                //howManyMinutes = it.date
+
+            }
+        }
+    }
 
     class EarthquakeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         var countryFlagIV : ImageView = itemView.findViewById(R.id.countryFlagIV)
         var countryTV : TextView = itemView.findViewById(R.id.countryTV)
         var intensityTV : TextView = itemView.findViewById(R.id.intensityTV)
+        var howManyMinutesTV : TextView = itemView.findViewById(R.id.HowManyMinutesTV)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EarthquakeViewHolder {
@@ -26,6 +57,6 @@ class EarthquakeRecyclerView : RecyclerView.Adapter<EarthquakeRecyclerView.Earth
     }
 
     override fun onBindViewHolder(holder: EarthquakeViewHolder, position: Int) {
-
+        //holder.howManyMinutesTV.text =
     }
 }
