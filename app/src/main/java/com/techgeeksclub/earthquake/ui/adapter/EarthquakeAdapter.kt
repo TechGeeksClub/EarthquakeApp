53490967ea8cf85d496e43f691dc58fdbead1a0a
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-01-02 18:40:41 +0300

    changed date format

--- app/src/main/java/com/techgeeksclub/earthquake/ui/adapter/EarthquakeAdapter.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/adapter/EarthquakeAdapter.kt
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.techgeeksclub.earthquake.data.entity.Earthquake
 import com.techgeeksclub.earthquake.data.entity.Result
 import com.techgeeksclub.earthquake.databinding.EarthquakeItemBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) : RecyclerView.Adapter<EarthquakeAdapter.HomePageItemHolder>(){
 
@@ -41,13 +43,26 @@ class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) 
     override fun onBindViewHolder(holder: HomePageItemHolder, position: Int) {
         val earthquake = earthquakeList.result
         val binding = holder.item
-
         result = earthquake
 
-        binding.countryTV.text = result[position].title.toString()
+        val formattedTime = formatToHourMinute(result[position].date.toString())
 
+        binding.countryTV.text = result[position].title.toString()
+        binding.intensityTV.text = result[position].mag.toString()
+        binding.dateTimeTV.text = formattedTime
 
 
 
     }
+
+    private fun formatToHourMinute(dateTime : String): String {
+        val inputFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        val date = inputFormat.parse(dateTime)
+        return outputFormat.format(date)
+    }
+
+
+
 }
