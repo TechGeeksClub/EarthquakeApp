a44b884f8cd56ad828e96b540b2351cf1f46b8c1
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-01-02 13:57:34 +0300

    added serializedName tag

--- app/src/main/java/com/techgeeksclub/earthquake/data/entity/EarthquakeResponse.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/data/entity/EarthquakeResponse.kt
@@ -1,7 +1,9 @@
 package com.techgeeksclub.earthquake.data.entity
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class EarthquakeResponse(
+    @SerializedName("")
     var earthquakes : List<Earthquake>
 ) : Serializable
