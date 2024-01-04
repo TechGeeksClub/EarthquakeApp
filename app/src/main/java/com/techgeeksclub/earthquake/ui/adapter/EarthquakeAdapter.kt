047c87f2a583b5e172d94408a43d59d8d47f8860
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-01-04 15:00:02 +0300

    calculating how many minutes passed after earthquake

--- app/src/main/java/com/techgeeksclub/earthquake/ui/adapter/EarthquakeAdapter.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/adapter/EarthquakeAdapter.kt
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.techgeeksclub.earthquake.data.entity.Earthquake
 import com.techgeeksclub.earthquake.data.entity.Result
 import com.techgeeksclub.earthquake.databinding.EarthquakeItemBinding
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) : RecyclerView.Adapter<EarthquakeAdapter.HomePageItemHolder>(){
@@ -16,20 +18,6 @@ class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) 
 
     var result : List<Result> = listOf()
 
-    init {
-        /*earthquake.forEach {
-            result = it.result
-            it.result.forEach {
-                val dateNow = LocalDateTime.now()
-                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                val formatted = dateNow.format(formatter)
-
-
-                //howManyMinutes = it.date
-
-            }
-        }*/
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomePageItemHolder {
         val binding = EarthquakeItemBinding.inflate(LayoutInflater.from(mContext), parent, false)
@@ -46,10 +34,12 @@ class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) 
         result = earthquake
 
         val formattedTime = formatToHourMinute(result[position].date.toString())
+        val minutesPassed = calculateMinutesPassed(result[position].date.toString())
 
         binding.countryTV.text = result[position].title.toString()
         binding.intensityTV.text = result[position].mag.toString()
         binding.dateTimeTV.text = formattedTime
+        binding.minutesPassedTV.text = "$minutesPassed minutes ago"
 
 
 
@@ -63,6 +53,14 @@ class EarthquakeAdapter (var mContext: Context, var earthquakeList: Earthquake) 
         return outputFormat.format(date)
     }
 
+    private fun calculateMinutesPassed(dateTime: String): Long{
+        val inputFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault())
+        val currentDate = Date()
+        val startDate = inputFormat.parse(dateTime)
+        val difference = currentDate.time - startDate.time
+
+        return abs(difference / (60 * 1000)) // toplam milisaniye farkını dakika olarak hesaplar
+    }
 
 
 }
