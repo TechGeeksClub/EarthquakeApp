6a5e2db96cc47c82f8c134d12f1afd8153b3dbc1
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-01-09 14:10:38 +0300

    adding pins for each earthquake

--- app/src/main/java/com/techgeeksclub/earthquake/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/fragment/HomeFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.techgeeksclub.earthquake.data.entity.Earthquake
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,9 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.techgeeksclub.earthquake.databinding.FragmentHomeBinding
-import com.techgeeksclub.earthquake.data.repository.EarthquakeRepository
 import com.techgeeksclub.earthquake.ui.adapter.EarthquakeAdapter
-import com.techgeeksclub.earthquake.ui.adapter.EarthquakeRecyclerView
 import com.techgeeksclub.earthquake.ui.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -66,22 +63,6 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 
         val mapFragment = childFragmentManager.findFragmentById(com.techgeeksclub.earthquake.R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
-       /* val earthquakeRepository = EarthquakeRepository()
-        earthquakeRepository.getEarthquakes(
-            onSuccess = {
-                it.result.forEach {
-                    Log.d("Eartquake",it.title.toString())
-                    Log.d("Eartquake",it.mag.toString())
-                    Log.d("Eartquake",it.depth.toString())
-
-
-                }
-            },
-            onFailure = {
-                Log.d("Eartquake",it.message.toString())
-
-            }
-        )*/
 
     }
 
@@ -92,14 +73,23 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(36.8732452, 30.8206177)
-        mMap!!.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        viewModel.earthquakes.observe(viewLifecycleOwner) { earthquakesList ->
+            mMap?.clear()
+            //Adding pins for each earthquake
+            earthquakesList.result.forEach { earthquake ->
+                val coordinates = earthquake.geojson?.coordinates
+                val latitude = coordinates?.get(1)
+                val longitude = coordinates?.get(0)
+
+                val location = LatLng(latitude!!, longitude!!)
+                mMap?.addMarker(
+                    MarkerOptions()
+                        .position(location)
+                        .title(earthquake.title)
+                )
+            }
+        }
     }
 
 }
