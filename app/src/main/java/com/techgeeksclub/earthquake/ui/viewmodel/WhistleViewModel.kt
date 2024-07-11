bc204f5a0fbbb266f0c48cdb8fc81d97e4638f73
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-07-11 17:52:06 +0300

    added whistle viewmodel

---
+++ app/src/main/java/com/techgeeksclub/earthquake/ui/viewmodel/WhistleViewModel.kt
@@ -0,0 +1,9 @@
+package com.techgeeksclub.earthquake.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WhistleViewModel @Inject constructor(): ViewModel(){
+}
